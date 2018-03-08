5a56326ca493191d7cf828020c892686634567d1
Bryon Nicoson <bryon.nicoson@gmail.com>
2018-03-08 08:43:36 -0600

    keyboard hidden, empty queryString and networkconnection issues handled

--- app/src/main/java/com/bryonnicoson/whowroteit/MainActivity.java
+++ app/src/main/java/com/bryonnicoson/whowroteit/MainActivity.java
@@ -1,8 +1,12 @@
 package com.bryonnicoson.whowroteit;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -23,7 +27,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void searchBooks(View view) {
-        String mQueryString = mBookInput.getText().toString();
-        new FetchBook(mTitleText, mAuthorText).execute(mQueryString);
+        // hide the keyboard
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+
+        // get network state
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        String queryString = mBookInput.getText().toString();
+        if (queryString.length() == 0) {
+            mTitleText.setText(R.string.empty_query_prompt);
+            mAuthorText.setText("");
+        } else {
+            if (networkInfo != null && networkInfo.isConnected()) {
+                new FetchBook(mTitleText, mAuthorText).execute(queryString);
+                mTitleText.setText(R.string.loading);
+                mAuthorText.setText("");
+            } else {
+                mTitleText.setText(R.string.network_error_prompt);
+                mAuthorText.setText("");
+            }
+        }
     }
 }
